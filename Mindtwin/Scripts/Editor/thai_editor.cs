c14256762e4dec8f86ecfe415c7650ee9ac90e1a
Ryan Martin <ryan@ensomniac.com>
2020-01-03 15:15:50 -0500

    Initial commit

---
+++ Mindtwin/Scripts/Editor/thai_editor.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+// Auto generated 1/3/2020
+using UnityEditor;
+namespace Editor {
+    public static class ThaiMindtwinMenu {
+        [MenuItem("Mindtwin/Load/Thai")]
+        public static void LoadThai() {
+            MindtwinMenu.Load("thai");
+        }
+    }
+}
+#endif
